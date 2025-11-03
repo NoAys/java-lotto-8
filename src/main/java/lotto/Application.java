4e161d9f28a21f443911ad99803879dfed599637
NoAys <solluna9@naver.com>
2025-11-03 21:00:29 +0900

    feat: 당첨 번호 및 보너스 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,6 +16,11 @@ public class Application {
         System.out.println(ticketCount + "개를 구매했습니다.");
         List<Lotto> lottos = buyLottos(ticketCount);
         lottos.forEach(lotto -> System.out.println(lotto.getNumbers()));
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumbers = readWinningNumbers();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        int bonus = readBonusNumber(winningNumbers);
     }
 
     // 구입금액 입력
@@ -48,5 +53,48 @@ public class Application {
             lottos.add(new Lotto(numbers));
         }
         return lottos;
+    }
+
+    // 보너스 번호 확인
+    private static List<Integer> readWinningNumbers(){
+        String input = Console.readLine();
+        try {
+            List<Integer> numbers = Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .toList();
+            validateWinningNumbers(numbers);
+            return numbers;
+        } catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 잘못된 당첨 번호 입력입니다.");
+        }
+    }
+
+    // 당첨번호 유효성 검사
+    private static void validateWinningNumbers(List<Integer> numbers) {
+        if (numbers.size() != 6){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 중복된 번호가 있습니다.");
+        }
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 번호는 1~45 사이여야 합니다.");
+        }
+    }
+
+    private static int readBonusNumber(List<Integer> winnngNumbers) {
+        try {
+            int bonus = Integer.parseInt(Console.readLine());
+            if (winnngNumbers.contains(bonus)) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복 될수 없습니다.");
+            }
+            if (bonus < 1 || bonus > 45) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+            }
+            return bonus;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+        }
     }
 }
