13f4eacb5ab3536cd41f07ab318de69056dabcec
NoAys <solluna9@naver.com>
2025-11-03 19:18:33 +0900

    feat: 구매 로또 개수 계산 및 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,7 +7,9 @@ public class Application {
         // TODO: 프로그램 구현
 
         System.out.println("구입금액을 입력해 주세요.");
-        int input = readPurchase();
+        int purchase = readPurchase();
+        int ticketCount = purchase / 1000;
+        System.out.println(ticketCount + "개를 구매했습니다.");
     }
 
     private static int readPurchase(){
