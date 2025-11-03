bec01d8b38fba560e8f4ea055158ec3f20b65b9d
NoAys <solluna9@naver.com>
2025-11-03 21:20:34 +0900

    refactor: Lotto 클래스 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -26,25 +26,9 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    // 당첨 번호와 일치하는 개수 계산
-    public int countMatching(List<Integer> winningNumbers){
-        return (int) numbers.stream()
-                .filter(winningNumbers::contains)
-                .count();
-    }
-
-    // 보너스 번호 포함 여부 확인
-    public boolean containBonus(int bonusNumber){
-        return numbers.contains(bonusNumber);
-    }
 
     // 로또 번호 반환
     public List<Integer> getNumbers() {
         return new ArrayList<>(numbers);
-    }
-
-    @Override
-    public String toString() {
-        return numbers.toString();
     }
 }
